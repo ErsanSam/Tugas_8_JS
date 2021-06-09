9d6e9edf2aa53ba476bd204d9fe778f37726df5f
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-09 08:44:30 +0700

    Tugas 8 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,12 @@
+function panggilMotor(){
+  var motor ={
+    type : "kopling",
+    harga : 17000000,
+    warna : 'biru',
+    tahun : 2009,
+  }
+  motor.tahun = 2008,
+  console.log(motor)
+  console.log(motor.tahun)
+}
+panggilMotor()
